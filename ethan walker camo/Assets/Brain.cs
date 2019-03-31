6d1f14fe93d39630c8199ebb23fa30a22efc87c1
aishwaryapothula <aishwarya.pothula@gmail.com>
2019-03-31 01:09:54 -0500

    ethan walker

---
+++ ethan walker camo/Assets/Brain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+
+//sits between the manager and the character listening from the manager
+// and telling the character what to do
+
+//adds required components as dependencies
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class Brain : MonoBehaviour
+{
+    public int DNALength = 1;
+    public float timeAlive;
+    public float distanceTravelled;
+    Vector3 startPosition;
+    //sequence of dna
+    public DNA dna;
+
+    //to access third person character script
+    private ThirdPersonCharacter m_Character;
+    private Vector3 m_Move;
+    private bool m_Jump;
+    bool alive = true;
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if(obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        // Intitialize DNA
+        // 0 forward
+        // 1 back
+        // 2 left
+        // 3 right
+        // 4 jump
+        // 5 crouch
+        // uses one int but has 6 values..DNA length has maxValue of 6
+        dna = new DNA(DNALength, 6);
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        timeAlive = 0;
+        alive = true;
+    }
+
+    private void FixedUpdate()
+    {
+        // read DNA
+        float h = 0;
+        float v = 0;
+        bool crouch = false;
+        if (dna.GetGene(0) == 1) v = 1;//forward
+        else if (dna.GetGene(0) == 1) v = -1;//backward
+        else if (dna.GetGene(0) == 2) v = -1;//left
+        else if (dna.GetGene(0) == 3) v = 1;//right
+        else if (dna.GetGene(0) == 4) m_Jump=true;
+        else if (dna.GetGene(0) == 1) crouch=true;
+
+        m_Move = v * Vector3.forward + h * Vector3.right;
+        m_Character.Move(m_Move, crouch, m_Jump);
+        m_Jump = false;
+        if (alive)
+        {
+            timeAlive += Time.fixedDeltaTime;
+            distanceTravelled = Vector3.Distance(this.transform.position, startPosition);
+        }
+       
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+}
---
+++ ethan walker camo/Assets/DNA.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//standard class not monobehaviour attached to brain
+public class DNA 
+{
+    //a list of different properties,different chromozomes of DNA
+    List<int> genes = new List<int>();
+    //keeps track of the list of attributes
+    int dnaLength = 0;
+    // limits the number of attribtes when initializing
+    int maxValues = 0;
+
+    //constructor DNA
+    public DNA(int l, int v)
+    {
+        dnaLength = l;
+        maxValues = v;
+        //random values for the length and maxValue
+        SetRandom();
+    }
+
+    public void SetRandom()
+    {
+        genes.Clear();
+        for(int i=0; i < dnaLength; i++)
+        {
+            //resets values by looping
+            genes.Add(Random.Range(0, maxValues));
+        }
+    }
+
+    //set int value for some attributes
+    public void SetInt(int pos, int value)
+    {
+        genes[pos] = value;
+    }
+
+    //passing on genes to children
+    public void Combine (DNA d1, DNA d2)
+    {
+        for(int i = 0; i < dnaLength; i++)
+        {
+            if(i < dnaLength / 2.0)
+            {
+                genes[i] = d1.genes[i];
+            }
+            else
+            {
+                genes[i] = d2.genes[i];
+            }
+
+        }
+    }
+
+    //mutation
+    public void Mutate()
+    {
+        genes[Random.Range(0, dnaLength)] = Random.Range(0, maxValues);
+    }
+
+    public int GetGene(int pos)
+    {
+        return genes[pos];
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
